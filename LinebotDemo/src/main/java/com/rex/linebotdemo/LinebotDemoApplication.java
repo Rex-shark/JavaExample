50e2ca7f51db95697bbcca7359953edf3c6a70bb
rex.liu <rex.liu@tist.com.tw>
2025-10-21 16:25:14 +0800

    新增linebot

---
+++ LinebotDemo/src/main/java/com/rex/linebotdemo/LinebotDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rex.linebotdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinebotDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinebotDemoApplication.class, args);
+    }
+
+}
---
+++ LinebotDemo/src/test/java/com/rex/linebotdemo/LinebotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rex.linebotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinebotDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
